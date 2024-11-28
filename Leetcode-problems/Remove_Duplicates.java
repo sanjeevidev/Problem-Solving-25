7ca418badff8c60820456f2307495a65392ba046
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2024-11-28 23:00:29 +0530

    Remove Duplicates#1

---
+++ Leetcode-problems/Remove_Duplicates.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+class Remove_Duplicates {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(stack.isEmpty()){
+                stack.push(ch);
+            }else{
+                if(stack.peek()==ch){
+                    stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (char ch : stack) {
+            result.append(ch);
+        }
+        
+        return result.toString();
+    }
+}
