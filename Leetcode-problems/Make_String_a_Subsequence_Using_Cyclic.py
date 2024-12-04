3c03f6901f34e7bfa42ebda138e99fbe05b40dbe
Sanjeevi Kumar V <96916376+sanjeevidev@users.noreply.github.com>
2024-12-04 23:46:13 +0530

    #2825

--- Leetcode-problems/Make_String_a_Subsequence_Using_Cyclic.py
+++ Leetcode-problems/Make_String_a_Subsequence_Using_Cyclic.py
@@ -1,3 +1,8 @@
+# You are given two 0-indexed strings str1 and str2.
+# In an operation, you select a set of indices in str1, and for each index i in the set, increment str1[i] to the next character cyclically. 
+# That is 'a' becomes 'b', 'b' becomes 'c', and so on, and 'z' becomes 'a'.
+# Return true if it is possible to make str2 a subsequence of str1 by performing the operation at most once, and false otherwise.
+
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
         str2_idx = 0
